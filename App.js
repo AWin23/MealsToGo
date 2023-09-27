3917bf6918feaf9a83cfe6b8526592d98da3a39e
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air.local>
2023-09-26 22:49:43 -0700

    first commit of this mealstogo app project before first part completes

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { RestaurantsScreen } from "./src/features/restaurant/screens/restaurant.screen";
+
+
+import { useFonts as useOswald, Oswald_400Regular} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular} from "@expo-google-fonts/lato";
+
+
+import { ThemeProvider } from "styled-components/native";
+import { theme } from './src/infastructure/theme';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 export default function App() {
+
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/features/restaurant/screens/restaurant.screen.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet, FlatList, SafeAreaView, } from 'react-native';
+import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+
+import { RestaurantsInfoCard } from "../components/restaurant-info-card";
+import { Spacer } from "../../../components/spacers/spacer.component";
+
+export const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  shadow-color: #000;
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.3;
+  shadow-radius: 2px;
+  elevation: 2; /* This property adds a shadow on Android */
+`;
+
+const RestaurantList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 16,
+    },
+})``;
+
+const SafeArea = styled.SafeAreaView`
+    flex: 1;
+    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+export const RestaurantsScreen = () => (
+    <SafeArea>
+        <SearchContainer>
+            <Searchbar placeholder="Search for Nearby Meals" />
+        </SearchContainer>
+        <RestaurantList
+            data={[
+                { name: 1 },
+                { name: 2 },
+                { name: 3 },
+                { name: 4 },
+                { name: 5 },
+                { name: 6 },
+                { name: 7 },
+                { name: 8 },
+                { name: 9 },
+                { name: 10 },
+                { name: 11 },
+                { name: 12 },
+                { name: 13 },
+                { name: 14 },
+            ]}
+            renderItem={() => (
+                <Spacer position="bottom" size="large">
+                    <RestaurantsInfoCard />
+                </Spacer>
+            )}
+            keyExtractor={(item) => item.name}
+            contentContainerStyle={{ padding: 16 }}
+        />
+    </SafeArea>
+)
+
+
