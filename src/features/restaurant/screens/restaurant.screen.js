55ddcc6c21b6f834981ec492c47955c068cfa7a8
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-6.local>
2023-10-14 15:26:22 -0700

    added activity/loading indicator

--- src/features/restaurant/screens/restaurant.screen.js
+++ src/features/restaurant/screens/restaurant.screen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StatusBar, StyleSheet, FlatList, SafeAreaView, } from 'react-native';
+import { View } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 
@@ -9,6 +10,7 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 import { Spacer } from "../../../components/spacers/spacer.component";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
 export const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -25,12 +27,34 @@ const RestaurantList = styled(FlatList).attrs({
     },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+    margin-left: -25px;
+`
+
+const LoadingContainer = styled.View`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+`;
+
 
 export const RestaurantsScreen = () => {
     const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-    console.log(error);
     return (
+        
         <SafeArea>
+            {isLoading && (
+                <LoadingContainer style={{ position: "absolute", top: "50%", left: "50%" }}>
+                    <Loading size={50} animating={true} color={MD2Colors.blue300} />
+                </LoadingContainer>
+            )}
+
+            {error && (
+                <ErrorContainer style={{ position: "absolute", top: "50%", left: "50%" }}>
+                    Unable to Find Nearby Data 🥲
+                </ErrorContainer>
+            )}
+
             <SearchContainer>
                 <Searchbar placeholder="Search for Nearby Meals" />
             </SearchContainer>
