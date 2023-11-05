e9a22659186f6e590e699280a4e84b739c46b4c2
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-8.local>
2023-11-05 14:05:08 -0800

    favourites context is added

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,7 +1,31 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react';
+import { Provider } from 'react-native-paper';
 
-export const FavouritesContext = () => {
+export const FavouritesContext = createContext();
+
+export const FavouritesContextProvider = ({ children }) => {
+    const [favourites, setFavourites] = useState([]);
+
+    const add = (restaurant) => {
+        setFavourites([...favourites, restaurant])
+    };
+
+    const remove = (restaurant) => {
+        const newFavourites = favourites.filter(
+            (x) => x.placeId !== restaurant.placeId
+        );
+
+        setFavourites(newFavourites);
+    }
+
     return (
-        <div>FavouritesContext</div>
+        <FavouritesContext.Provider
+        value={{
+            favourites,
+            addToFavourites: remove,  
+        }}
+        >
+            {children}
+        </FavouritesContext.Provider>
     )
 }
